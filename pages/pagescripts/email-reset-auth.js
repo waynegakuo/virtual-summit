9eda74243c4d8f791f95ca7368fa1d8e43b91369
waynegakuo <waynejotham@gmail.com>
2020-05-30 22:27:29 +0300

    Reset password functionality done

---
+++ pages/pagescripts/email-reset-auth.js
@@ -0,0 +1,25 @@
+const passreset_modal_container = document.getElementById('passreset_modal_container');
+const passreset_close = document.getElementById('passreset_close');
+/**
+ * Send Email Reset to User
+ */
+const emailResetForm = document.querySelector('.reset-actions');
+
+emailResetForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const resetEmail = document.querySelector('#reset-email').value;
+
+    auth.sendPasswordResetEmail(resetEmail).then(function () {
+        // Email sent.
+        passreset_modal_container.classList.add('show'); // trigger the opening of the modal
+    }).catch(err => {
+        emailResetForm.querySelector('.error').innerHTML = err.message;
+    });
+})
+
+
+// close modal triggered by button
+passreset_close.addEventListener('click', () => {
+    passreset_modal_container.classList.remove('show');
+    window.location.href = 'interview.html'
+});
